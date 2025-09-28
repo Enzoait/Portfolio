803432a69f7ffbf4a05bcd59e5d9cc8e01b69769
Enzoait <pcenzox25@gmail.com>
2025-09-28 16:15:34 +0200

    header added lang switch

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -4,6 +4,9 @@ import Image from "next/image";
 import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
 import { LINKS, SOCIALS } from "@/constants/links";
 import { MobileMenuToggle } from "./ui/mobile-menu-toggle";
+import { HyperText } from "@/components/ui/hyper-text"
+import Flag from "react-flagpack";
+import Dropdown from "@/components/ui/dropdown";
 
 export default async function Header({
   params,
@@ -17,73 +20,80 @@ export default async function Header({
       <nav className="bg-transparent dark:bg-transparent fixed w-full z-20 top-0 start-0 border-b border-border dark:border-border">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link href={`/${lang}`}>
-        <p className="text-lg font-semibold">Enzo AÏT-YAKOUB</p>
+            <HyperText className="text-lg font-semibold">
+              Enzo AÏT-YAKOUB
+            </HyperText>
           </Link>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-        <div className="flex gap-4 self-center">
-          <Link
-            href={SOCIALS.github}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-          src="/github.svg"
-          alt="GitHub"
-          width={25}
-          height={25}
-          className="dark:hidden"
-            />
-            <Image
-          src="/github-white.svg"
-          alt="GitHub"
-          width={25}
-          height={25}
-          className="hidden dark:block"
-            />
-          </Link>
-          <Link
-            href={SOCIALS.linkedin}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-          src="/linkedin.svg"
-          alt="LinkedIn"
-          width={25}
-          height={25}
-          className="dark:hidden"
-            />
-            <Image
-          src="/linkedin-white.svg"
-          alt="LinkedIn"
-          width={25}
-          height={25}
-          className="hidden dark:block"
-            />
-          </Link>
-          <AnimatedThemeToggler />
-        </div>
-        <MobileMenuToggle targetId="navbar-sticky" text={dict.menu.open} />
+            <div className="flex gap-4 self-center">
+              <Link
+                href={SOCIALS.github}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="self-center"
+              >
+                <Image
+                  src="/github.svg"
+                  alt="GitHub"
+                  width={25}
+                  height={25}
+                  className="dark:hidden"
+                />
+                <Image
+                  src="/github-white.svg"
+                  alt="GitHub"
+                  width={25}
+                  height={25}
+                  className="hidden dark:block"
+                />
+              </Link>
+              <Link
+                href={SOCIALS.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="self-center"
+              >
+                <Image
+                  src="/linkedin.svg"
+                  alt="LinkedIn"
+                  width={25}
+                  height={25}
+                  className="dark:hidden"
+                />
+                <Image
+                  src="/linkedin-white.svg"
+                  alt="LinkedIn"
+                  width={25}
+                  height={25}
+                  className="hidden dark:block"
+                />
+              </Link>
+              <AnimatedThemeToggler />
+              <Dropdown>
+
+              </Dropdown>
+            </div>
+            <MobileMenuToggle targetId="navbar-sticky" text={dict.menu.open} />
           </div>
           <div
-        className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-        id="navbar-sticky"
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-sticky"
           >
-        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
-          <li>
-            <Link href={`/${lang}${LINKS.about}`}>{dict.links.about}</Link>
-          </li>
-          <li>
-            <Link href={`/${lang}${LINKS.projects}`}>
-          {dict.links.projects}
-            </Link>
-          </li>
-          <li>
-            <Link href={`/${lang}${LINKS.contact}`}>
-          {dict.links.contact}
-            </Link>
-          </li>
-        </ul>
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
+              <li>
+                <Link href={`/${lang}${LINKS.about}`}>{dict.links.about}</Link>
+              </li>
+              <li>
+                <Link href={`/${lang}${LINKS.projects}`}>
+                  {dict.links.projects}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${lang}${LINKS.contact}`}>
+                  {dict.links.contact}
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
