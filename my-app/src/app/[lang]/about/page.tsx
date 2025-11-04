69be2922508d932e4fb9a72de92b515ea19d480c
Enzoait <pcenzox25@gmail.com>
2025-11-04 10:36:51 +0100

    page about

--- my-app/src/app/[lang]/about/page.tsx
+++ my-app/src/app/[lang]/about/page.tsx
@@ -11,58 +11,66 @@ export default async function Page({
 }) {
   const { lang } = await params;
   const dict = await getDictionary(lang);
+
   return (
     <>
       <Header params={params} />
-      <div className="min-h-screen w-full flex flex-col items-center justify-center gap-4">
-        <div className="gap-6 flex flex-col items-center">
+
+      <div className="min-h-screen w-full flex flex-col items-center justify-start relative">
+        <div className="flex flex-col items-center gap-6 mt-35">
           <p className="text-3xl font-bold">{dict.aboutTitle}</p>
           <p className="text-[18px] w-[75%] text-center">
             {dict.aboutDescription}
           </p>
         </div>
         <LightRays />
-        <OrbitingCircles iconSize={40}>
-          <Image
-            src="/typescript.svg"
-            alt="Typescript"
-            width={40}
-            height={40}
-          />
-          <Image src="/react.svg" alt="React" width={40} height={40} />
-          <Image src="/python.svg" alt="Python" width={40} height={40} />
-          <Image src="/expo.svg" alt="Expo" width={40} height={40} />
-          <Image src="/csharp.svg" alt="CSharp" width={40} height={40} />
-          <Image src="/java.svg" alt="Java" width={40} height={40} />
-          <Image src="/tailwindcss.svg" alt="Tailwind" width={40} height={40} />
-          <Image src="/nextjs.svg" alt="Next" width={40} height={40} />
-        </OrbitingCircles>
-        <OrbitingCircles iconSize={30} radius={100} reverse speed={2}>
-          <Image
-            src="/path/to/your/image.png"
-            alt="Description"
-            width={30}
-            height={30}
-          />
-          <Image
-            src="/path/to/your/image.png"
-            alt="Description"
-            width={30}
-            height={30}
-          />
-          <Image
-            src="/path/to/your/image.png"
-            alt="Description"
-            width={30}
-            height={30}
-          />
-          <Image
-            src="/path/to/your/image.png"
-            alt="Description"
-            width={30}
-            height={30}
-          />
-        </OrbitingCircles>
+        <div className="relative flex items-center justify-cente gap-50">
+          <p className="text-3xl font-bold">{dict.technologiesTitle}</p>
+          <div className="relative size-[400px] flex items-center justify-center">
+            <OrbitingCircles iconSize={50}>
+              <Image
+                src="/typescript.svg"
+                alt="Typescript"
+                width={50}
+                height={50}
+              />
+              <Image src="/react.svg" alt="React" width={50} height={50} />
+              <Image src="/python.svg" alt="Python" width={50} height={50} />
+              <Image src="/expo.svg" alt="Expo" width={50} height={50} />
+              <Image src="/csharp.svg" alt="CSharp" width={50} height={50} />
+              <Image src="/java.svg" alt="Java" width={50} height={50} />
+              <Image
+                src="/tailwindcss.svg"
+                alt="Tailwind"
+                width={50}
+                height={50}
+              />
+              <Image src="/nextjs.svg" alt="Next" width={50} height={50} />
+            </OrbitingCircles>
+
+            <OrbitingCircles iconSize={50} radius={100} reverse speed={2}>
+              <Image
+                src="/googlecloud.svg"
+                alt="Google Cloud"
+                width={40}
+                height={40}
+              />
+              <Image
+                src="/postgresql.svg"
+                alt="PostgreSQL"
+                width={40}
+                height={40}
+              />
+              <Image src="/git.svg" alt="Git" width={40} height={40} />
+              <Image
+                src="/supabase.svg"
+                alt="Supabase"
+                width={40}
+                height={40}
+              />
+            </OrbitingCircles>
+          </div>
+        </div>
       </div>
     </>
   );
